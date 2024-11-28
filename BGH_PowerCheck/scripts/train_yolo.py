d9b590f1f0f514de77a2f42dd68ebf208538e598
edfedo <123000583+edfedo@users.noreply.github.com>
2024-11-28 18:43:45 +0100

    update

--- BGH_PowerCheck/scripts/train_yolo.py
+++ BGH_PowerCheck/scripts/train_yolo.py
@@ -3,14 +3,16 @@ from ultralytics import YOLO
 def main():
     model = YOLO('yolov8m.pt')  # Carga el modelo YOLO
     model.train(
-        data='dataset.yaml',  # Archivo de configuración del dataset
-        epochs=100,            # Número de épocas
-        batch=1,             # Tamaño del batch
+        #data='dataset.yaml',  # Archivo de configuración del dataset
+        data='C:/Practicas_Profesionalizantes_2_2024/BGH_PowerCheck/scripts/dataset.yaml',
+        epochs=1,            # Número de épocas
+        batch=16,             # Tamaño del batch
         imgsz=640,            # Tamaño de las imágenes
         save=True,            # Asegura que los pesos se guarden
         save_period=1,        # Guarda los pesos después de cada época
         project="runs/detect", # Carpeta de salida
-        name='cable_detector'
+        #name='cable_detector'
+        name='placa_detector'
     )
 
 if __name__ == "__main__":
